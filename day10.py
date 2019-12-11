5b75249bf3c3f871ca5f345ed37e2db9ee6821a2
bastien.laby <bastien.laby@teamto.com>
2019-12-11 15:40:06 +0100

    day 10 puzzle 2 wip

--- day10.py
+++ day10.py
@@ -14,19 +14,18 @@ Pour chaque astéroïde
    Afficher somme du set
 '''
 
-def normalize(vec2):
-    norm = math.sqrt(vec2[0] ** 2 + vec2[1] ** 2)
-    vec2[0] /= norm
-    vec2[1] /= norm
-    return vec2
+def norm(V):
+    return math.sqrt(V[0] ** 2 + V[1] ** 2)
+
+def normalized(V):
+    return [i / norm(V) for i in V]
 
 def getMaxAsteroidSeen(spaceMap):
 
-    width = len(spaceMap)
-    height = len(spaceMap[0])
+    asteroids = [(i, j) for i, j in itertools.product(range(len(spaceMap)), range(len(spaceMap[0]))) if spaceMap[j][i] == '#']
 
-    asteroids = [(i, j) for i, j in itertools.product(range(width), range(height)) if spaceMap[i][j] == '#']
     maxCount = 0
+    maxI, maxJ = None, None
 
     for i, j in asteroids:
 
@@ -37,24 +36,69 @@ def getMaxAsteroidSeen(spaceMap):
             if i == i2 and j == j2:
                 continue
 
-            direction = normalize([i2 - i, j2 - j])
+            direction = normalized([i2 - i, j2 - j])
             direction = [float('%.05f' % k) for k in direction]
             directions.add(tuple(direction))
 
         if len(directions) > maxCount:
             maxCount = len(directions)
+            maxI, maxJ = i, j
 
-    return maxCount
+    return maxCount, maxI, maxJ
 
 
 if __name__ == '__main__':
 
-    assert getMaxAsteroidSeen('.#..#\n.....\n#####\n....#\n...##'.split('\n')) == 8
-    assert getMaxAsteroidSeen('......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####'.split('\n')) == 33
-    assert getMaxAsteroidSeen('#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.'.split('\n')) == 35
-    assert getMaxAsteroidSeen('.#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..'.split('\n')) == 41
-    assert getMaxAsteroidSeen('.#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##'.split('\n')) == 210
+    # assert getMaxAsteroidSeen('.#..#\n.....\n#####\n....#\n...##'.split('\n'))[0] == 8
+    # assert getMaxAsteroidSeen('......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####'.split('\n'))[0] == 33
+    # assert getMaxAsteroidSeen('#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.'.split('\n'))[0] == 35
+    # assert getMaxAsteroidSeen('.#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..'.split('\n'))[0] == 41
+    # assert getMaxAsteroidSeen('.#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##'.split('\n'))[0] == 210
 
     with open(__file__.replace('.py', '.input'), 'r') as f:
          spaceMap = [i.strip() for i in f.readlines()]
-    print(getMaxAsteroidSeen(spaceMap))
+    mostEfficientAsteroid = getMaxAsteroidSeen(spaceMap)
+    print(mostEfficientAsteroid)
+
+    laserI, laserJ = mostEfficientAsteroid[1:]
+    asteroids = [(i, j) for i, j in itertools.product(range(len(spaceMap)), range(len(spaceMap[0]))) if spaceMap[j][i] == '#' and i != laserI and j != laserJ]
+
+    asteroidsByAngle = {} # key = angle, value = list of directions
+    for i, j in asteroids:
+        D = [i - laserI, j - laserJ]
+        nD = normalized(D)
+        angle = math.acos(nD[0]) if nD[1] > 0 else - math.acos(nD[0])
+        angle -= math.pi / 2.0 # remap from [-pi, pi] to [-3pi/2, 3pi/2]
+        angle = (angle + math.pi) % (2 * math.pi) # remap from [-3pi/2, 3pi/2] to [0, 2pi]
+        asteroidsByAngle.setdefault(angle, [])
+        asteroidsByAngle[angle].append((nD, i, j))
+
+    for angle, asteroids in asteroidsByAngle.items():
+        asteroids.sort(key=lambda a: norm(a[0]))
+
+    # for angle in sorted(asteroidsByAngle.keys()):
+    #     print(angle, asteroidsByAngle[angle])
+
+    assert sum([len(i) for i in asteroidsByAngle.values()]) >= 200
+
+    killedAsteroids = 0
+    lastAsteroidKilled = None
+
+    laserLoop = 1
+    while True:
+        print('Laser Loop %s' % laserLoop)
+        breakLoop = False
+        for angle in sorted(asteroidsByAngle.keys()):
+            if asteroidsByAngle[angle]:
+                lastAsteroidKilled = asteroidsByAngle[angle].pop(0)
+                killedAsteroids += 1
+                # print('Kill asteroid %s (%sth)' % (lastAsteroidKilled, killedAsteroids))
+            if killedAsteroids == 200:
+                breakLoop = True
+                break
+        if breakLoop:
+            break
+
+        laserLoop += 1
+
+    print(lastAsteroidKilled)
