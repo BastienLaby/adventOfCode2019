a98fbfe4774425ab0efb5e5cd879d953a894abd4
bastien.laby <bastien.laby@teamto.com>
2019-12-11 16:31:49 +0100

    Day 10 but very ugly, need refacto

--- day10.py
+++ day10.py
@@ -5,20 +5,13 @@ import math
 import itertools
 
 
-'''
-Pour chaque astéroïde
-   Initialiser un set de tuples de directions
-   Pour chaque autre astéroïde
-      Calculer la direction normalisée
-      L'ajouter au set
-   Afficher somme du set
-'''
-
 def norm(V):
     return math.sqrt(V[0] ** 2 + V[1] ** 2)
 
+
 def normalized(V):
     return [i / norm(V) for i in V]
+
 
 def getMaxAsteroidSeen(spaceMap):
 
@@ -37,7 +30,7 @@ def getMaxAsteroidSeen(spaceMap):
                 continue
 
             direction = normalized([i2 - i, j2 - j])
-            direction = [float('%.05f' % k) for k in direction]
+            direction = [float('%.06f' % k) for k in direction]
             directions.add(tuple(direction))
 
         if len(directions) > maxCount:
@@ -49,50 +42,56 @@ def getMaxAsteroidSeen(spaceMap):
 
 if __name__ == '__main__':
 
-    # assert getMaxAsteroidSeen('.#..#\n.....\n#####\n....#\n...##'.split('\n'))[0] == 8
-    # assert getMaxAsteroidSeen('......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####'.split('\n'))[0] == 33
-    # assert getMaxAsteroidSeen('#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.'.split('\n'))[0] == 35
-    # assert getMaxAsteroidSeen('.#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..'.split('\n'))[0] == 41
-    # assert getMaxAsteroidSeen('.#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##'.split('\n'))[0] == 210
+    spaceMap = '.#..#\n.....\n#####\n....#\n...##'.split('\n')
+    assert getMaxAsteroidSeen(spaceMap)[0] == 8
+    spacemap = '......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####'.split('\n')
+    assert getMaxAsteroidSeen(spacemap)[0] == 33
+    spacemap = '#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.'.split('\n')
+    assert getMaxAsteroidSeen(spacemap)[0] == 35
+    spaceMap = '.#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..'.split('\n')
+    assert getMaxAsteroidSeen(spaceMap)[0] == 41
+    spaceMap = '.#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##'.split('\n')
+    assert getMaxAsteroidSeen(spaceMap)[0] == 210
 
     with open(__file__.replace('.py', '.input'), 'r') as f:
          spaceMap = [i.strip() for i in f.readlines()]
+
     mostEfficientAsteroid = getMaxAsteroidSeen(spaceMap)
     print(mostEfficientAsteroid)
 
-    laserI, laserJ = mostEfficientAsteroid[1:]
-    asteroids = [(i, j) for i, j in itertools.product(range(len(spaceMap)), range(len(spaceMap[0]))) if spaceMap[j][i] == '#' and i != laserI and j != laserJ]
+    O = mostEfficientAsteroid[1:]
+    asteroids = [(i, j) for i, j in itertools.product(range(len(spaceMap)), range(len(spaceMap[0]))) if spaceMap[j][i] == '#' and not (i == O[0] and j == O[1])]
+
+    def getAngleFromVector(i, j):
+        angle = math.acos(i) if j > 0 else - math.acos(i)
+        angle = (angle + math.pi) % (2 * math.pi)
+        angle = (angle - math.pi / 2.0) % (2 * math.pi)
+        return angle
 
     asteroidsByAngle = {} # key = angle, value = list of directions
-    for i, j in asteroids:
-        D = [i - laserI, j - laserJ]
+    for A in asteroids:
+        D = [A[0] - O[0], A[1] - O[1]]
         nD = normalized(D)
-        angle = math.acos(nD[0]) if nD[1] > 0 else - math.acos(nD[0])
-        angle -= math.pi / 2.0 # remap from [-pi, pi] to [-3pi/2, 3pi/2]
-        angle = (angle + math.pi) % (2 * math.pi) # remap from [-3pi/2, 3pi/2] to [0, 2pi]
+        angle = getAngleFromVector(nD[0], nD[1])
+        angle = float('%.06f' % angle)
         asteroidsByAngle.setdefault(angle, [])
-        asteroidsByAngle[angle].append((nD, i, j))
+        asteroidsByAngle[angle].append((D, A[0], A[1]))
 
-    for angle, asteroids in asteroidsByAngle.items():
-        asteroids.sort(key=lambda a: norm(a[0]))
-
-    # for angle in sorted(asteroidsByAngle.keys()):
-    #     print(angle, asteroidsByAngle[angle])
-
-    assert sum([len(i) for i in asteroidsByAngle.values()]) >= 200
+    # sort asteroids of each angle by distance
+    for angle, asteroidList in asteroidsByAngle.items():
+        asteroidList.sort(key=lambda a: norm(a[0]))
 
     killedAsteroids = 0
     lastAsteroidKilled = None
 
     laserLoop = 1
     while True:
-        print('Laser Loop %s' % laserLoop)
         breakLoop = False
         for angle in sorted(asteroidsByAngle.keys()):
             if asteroidsByAngle[angle]:
                 lastAsteroidKilled = asteroidsByAngle[angle].pop(0)
                 killedAsteroids += 1
-                # print('Kill asteroid %s (%sth)' % (lastAsteroidKilled, killedAsteroids))
+                print('Kill asteroid %s a=%s (%sth)' % (lastAsteroidKilled[1:], angle, killedAsteroids))
             if killedAsteroids == 200:
                 breakLoop = True
                 break
@@ -100,5 +99,3 @@ if __name__ == '__main__':
             break
 
         laserLoop += 1
-
-    print(lastAsteroidKilled)
